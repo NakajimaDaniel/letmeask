39f8d0d82b1b60728aaddf3640952222e3f2066d
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-06-29 20:19:01 -0300

    feat: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { NewRoom } from './pages/NewRoom';
 import { Home } from './pages/Home';
+
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Home /> 
+    <BrowserRouter>
+      <Route path="/" exact component={Home} /> 
+      <Route path="/rooms/new" component={NewRoom} /> 
+    </BrowserRouter>
+    
   );
 }
 
