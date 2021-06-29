8c969d45a5442d1f20c282f7c338bc2f72cc5f3d
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-06-29 19:51:11 -0300

    feat: new room page

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,37 @@
+
+import illustrationImg from '../assets/illustration.svg'
+import logoImg from '../assets/logo.svg'
+import googleImg from '../assets/google-icon.svg'
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} ></img>
+        <strong>Crie salas ao vivo</strong>
+        <p>Tire as duvidas em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg}></img>
+
+          <h2>Criar nova sala</h2>
+
+          <form>
+            <input 
+              type="text" 
+              placeholder="nome da sala"
+
+            />
+            <Button type="submit"> criar sala </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente ? <a href="#"> clique aqui </a>
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
