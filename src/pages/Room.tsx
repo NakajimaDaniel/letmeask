e94fdc51373b476a1e541d7ac32a326d596e4b2f
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-07-03 17:08:52 -0300

    feat: room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,6 +1,36 @@
 
+import LogoImg from '../assets/logo.svg'
+import { Button } from '../components/Button'
+import '../styles/room.scss'
+
 export function Room() {
   return (
-    <div></div>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={LogoImg} alt="letmeask" />
+          <div>ddddd</div>
+
+        </div>
+      </header>
+      <main>
+        <div className="room-title">
+          <h1>sala</h1>
+          <span>4 perguntas0</span>
+
+        </div>
+
+        <form >
+          <textarea placeholder="pergunta" /> 
+
+          <div className="form-footer">
+            <span>para enviar uma pergunta, <button>faça seu login</button> </span>
+            <Button type="submit">Enviar pergunta </Button>
+          </div>
+        </form>
+
+      </main>
+
+    </div>
   )
 }
