8c5990b866731dfc3c5bd57de4f1bdf88e2561fc
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-07-05 19:03:56 -0300

    feat: admin room

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,85 @@
+
+import { FormEvent, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import LogoImg from '../assets/logo.svg'
+import { Button } from '../components/Button'
+import { Question } from '../components/Question'
+import { RoomCode } from '../components/RoomCode'
+import { useAuth } from '../hooks/useAuth'
+import { useRoom } from '../hooks/useRoom'
+import { database } from '../services/firebase'
+import '../styles/room.scss'
+
+type roomParams = {
+  id: string;
+}
+
+
+
+
+export function AdminRoom() {
+
+  const { user } = useAuth();
+  const params = useParams<roomParams>();
+  const roomId = params.id;
+  const [newQuestion, setNewQuestion] = useState('');
+  const { title, questions } = useRoom(roomId);
+
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+    if (newQuestion.trim() === '') {
+      return;
+    }
+
+    if (!user) {
+      throw new Error('you must be logged in')
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+    setNewQuestion('');
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={LogoImg} alt="letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="room-title">
+          <h1>{title}</h1>
+          {questions.length > 0 && <span> {questions.length} pergunta(s)</span>}
+
+        </div>
+
+        
+        <div className="question-list">
+          {questions.map(question => {
+            return (
+              <Question key={question.id} content={question.content} author={question.author} /> 
+            )
+          })}
+        </div>
+
+      </main>
+
+    </div>
+  )
+}
