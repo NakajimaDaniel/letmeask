9280a148a1c539490d7d29285fc11b3f6b1eaad7
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-07-15 20:32:21 -0300

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+   
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        gray: '#a8a8b3',
+        red: '#ea4335',
+        purple: '#835afd',
+        white: '#ffff',
+      },
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
+
