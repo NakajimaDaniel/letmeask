e847ff62d31c626a08896a62c38b20530707076c
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-07-19 19:21:10 -0300

    fix: added dark mode variant

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,6 +1,6 @@
 
 import copyImg from '../assets/copy.svg';
-import '../styles/room-code.scss';
+// import '../styles/room-code.scss';
 
 type roomCodeProps = {
   code: string;
@@ -21,13 +21,13 @@ export function RoomCode(props: roomCodeProps) {
     //   <span>{props.code} </span>
     // </button>
     
-    <button className="flex items-between border border-purple rounded-md bg-white "   onClick={copyRoomCodeToClipboard} >
+    <button className="flex items-between border border-purple rounded-md bg-white dark:bg-black-500"   onClick={copyRoomCodeToClipboard} >
       <div className="flex items-between">
       <div className=" flex items-center md:p-3 bg-purple">
         <img src={copyImg} alt="copy" />
       </div>
 
-      <span className="flex items-center pl-3 pr-3 w-20 break-all  md:break-normal md:w-full md:overflow-clip " >
+      <span className="flex items-center pl-3 pr-3 w-20 break-all  md:break-normal md:w-full md:overflow-clip dark:text-white" >
         { props.code }
       </span>
       </div>
