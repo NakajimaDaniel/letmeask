704d8bf3c424bb7c70149497cf962e14e92c4a7a
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-07-19 18:53:56 -0300

    feat: added themeContextProvider to app

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { Room } from './pages/Room';
 import { AdminRoom } from './pages/AdminRoom';
 
 import "tailwindcss/tailwind.css"
+import { ThemeContextProvider } from './contexts/ThemeContext';
 
 
 function App() {
@@ -20,16 +21,15 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Switch>
-          <Route path="/" exact component={Home} /> 
-          <Route path="/rooms/new" component={NewRoom} /> 
-          <Route path="/rooms/:id" component={Room} /> 
-          <Route path="/admin/rooms/:id" component={AdminRoom} /> 
-        </Switch>
-
+        <ThemeContextProvider>
+          <Switch>
+            <Route path="/" exact component={Home} /> 
+            <Route path="/rooms/new" component={NewRoom} /> 
+            <Route path="/rooms/:id" component={Room} /> 
+            <Route path="/admin/rooms/:id" component={AdminRoom} /> 
+          </Switch>
+        </ThemeContextProvider>
       </AuthContextProvider>
-
-
     </BrowserRouter>
     
   );
