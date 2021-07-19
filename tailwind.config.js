38b461ff259f38f598b8d32da8e9ab23f6997444
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-07-19 19:16:50 -0300

    fix: color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,16 @@ module.exports = {
           700: '#737380',
           500: '#a8a8b3',
           400: '#e2e2e2',
+          300: '#f8f8f8',
         },
         red: '#ea4335',
         purple: '#835afd',
         white: '#ffff',
         pink: {
           500: '#e559f9',
+        },
+        black: {
+          500: '#232323',
         },
       },
       fontFamily: {
