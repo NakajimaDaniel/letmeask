f0f2bee03f17eab372550a02116cdbd811bbd26f
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-07-21 19:34:53 -0300

    fix: dark mode variant

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -16,13 +16,13 @@ type QuestionProps = {
 
 export function Question({ content, author, children, isHighlighted = false, isAnswered = false }: QuestionProps) {
   return (
-    <div className={`question ${isAnswered ? 'answered'  : '' } ${isHighlighted && !isAnswered ? 'hightlighted' : '' } bg-gray-200 rounded-lg p-6 mt-2 shadow-md mb-5`}>
-      <p  className="text-black-400" >{content}</p>
+    <div className={`question ${isAnswered ? 'answered'  : '' } ${isHighlighted && !isAnswered ? 'hightlighted' : '' } bg-gray-200 rounded-lg p-6 mt-2 shadow-md mb-5 dark:bg-gray-600`}>
+      <p  className="text-black-400 dark:text-gray-400" >{content}</p>
       <footer  className="flex justify-between items-center mt-8" >
         <div className="flex items-center">
           <img className="w-8 h-8 rounded-full "
             src={author.avatar} alt={author.name} />
-          <span className="ml-2  text-gray-700"  >{author.name}</span>
+          <span className="ml-2  text-gray-700 dark:text-gray-400 "  >{author.name}</span>
         </div>
         <div>
           {children}
