8dc72fe868ae42fc1654250eb4349766babe8586
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-07-24 18:56:05 -0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,24 @@ module.exports = {
       colors: {
         gray: {
           700: '#737380',
+          600: '#545454',
           500: '#a8a8b3',
           400: '#e2e2e2',
           300: '#f8f8f8',
+          200: '#fefefe',
         },
         red: '#ea4335',
         purple: '#835afd',
         white: '#ffff',
         pink: {
+          900: '#391038',
+          700: '#B70BB0',
           500: '#e559f9',
+          100: '#F4F0FF',
         },
         black: {
           500: '#232323',
+          400:'#29292e',
         },
       },
       fontFamily: {
